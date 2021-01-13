6f07e9c84434c901e45c3b543c027c602593991c
Viet Anh <vietanhjp93@gmail.com>
2021-01-13 22:25:05 +0900

    checkbook count diable done!

--- src/app/step-html/step-html.component.ts
+++ src/app/step-html/step-html.component.ts
@@ -9,6 +9,7 @@ import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class StepHtmlComponent implements OnInit {
 
   formGroup: FormGroup;
+  public countChecked = 0;
 
   constructor(
     private formBuilder: FormBuilder
@@ -19,9 +20,53 @@ export class StepHtmlComponent implements OnInit {
    }
 
   
+   sbook = '978-4-7741-9130-0';
+   // ［2］チェックボックスの状態を管理
+   books: Array<{isbn: string, title: string, selected: boolean, disabled: boolean}> = [];
+   book = [
+    { isbn: '978-4-8222-9894-4', title: '基礎からしっかり学ぶC#の教科書',
+      selected: false, disabled: false },
+    { isbn: '978-4-8222-5355-4', title: 'アプリを作ろう！ Visual C#入門',
+      selected: false, disabled: false },
+    { isbn: '978-4-7741-9130-0', title: 'Angularアプリケーションプログラミング',
+      selected: true, disabled: false },
+    { isbn: '978-4-7741-9030-3', title: 'C#ポケットリファレンス',
+      selected: false, disabled: false },
+    { isbn: '978-4-7741-8994-9', title: 'HTML&CSS 超入門',
+      selected: false, disabled: false }
+  ];
   checkbox: any;
 
+  show() {
+    let count = 0
+    this.books.forEach((item, index) => {
+      if (item.selected) {
+        count++;
+      }
+    });
+    this.countChecked = count;
+    console.log(this.countChecked);
+
+    
+    let selected = this.books.filter((item) => item.selected);
+    console.log(selected);
+  }
   ngOnInit(): void {
+    this.book.forEach(a => this.books.push(a));
+    this.book.forEach(a => this.books.push(a));
+    this.book.forEach(a => this.books.push(a));
+    this.books.forEach((item, index) => {
+      if (item.selected) {
+        this.countChecked++;
+      }
+    });
+
+
+
+
+
+
+
     let arr: Array<{id : number, name : string, check: boolean, disabled: boolean}> = [ 
       {id:1, name : "a", check: true, disabled: false},
       {id:2, name : "b", check: false, disabled: false},
